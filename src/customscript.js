1e8813f680d903ae084db2c2dcde14f53f6a2e8e
Sagun Chauhan <st.sagunchauhan1@gmail.com>
2021-10-20 17:17:35 +0530

    Alert "Hello" removed from customscript.js file.

--- src/customscript.js
+++ src/customscript.js
@@ -1,7 +1,11 @@
 (function e() {
-    alert('Hello');
-    var e = document.createElement("script");
-    (e.type = "text/javascript"), (e.async = true), (e.src = "//staticw2.yotpo.com/jV57gp4RKXABm7N2zd94UnPq6pgfRerQyaeq3iQk/widget.js");
-    var t = document.getElementsByTagName("script")[0];
-    t.parentNode.insertBefore(e, t);
+var e = document.createElement("script"); e.type = "text/javascript", e.async = true,
+e.src = "//staticw2.yotpo.com/jV57gp4RKXABm7N2zd94UnPq6pgfRerQyaeq3iQk/widget.js";
+var t = document.getElementsByTagName("script")[0]; t.parentNode.insertBefore(e, t)
 })();
+setTimeout(function () {
+debugger;
+var api = new Yotpo.API(yotpo);
+console.log("api", api);
+api.refreshWidgets();
+}, 500);
