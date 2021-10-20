f792e2501dbd5817bb6d16f355436e40f985077f
Sagun Chauhan <st.sagunchauhan1@gmail.com>
2021-10-20 15:30:08 +0530

    customscript file inculded

---
+++ src/customscript.js
@@ -0,0 +1,7 @@
+(function e() {
+    alert('Hello');
+    var e = document.createElement("script");
+    (e.type = "text/javascript"), (e.async = true), (e.src = "//staticw2.yotpo.com/jV57gp4RKXABm7N2zd94UnPq6pgfRerQyaeq3iQk/widget.js");
+    var t = document.getElementsByTagName("script")[0];
+    t.parentNode.insertBefore(e, t);
+})();
