ca175edaf32caa6ad1b01c23b420847a3fe11c5d
st-dotnet <stdev.net@gmail.com>
2021-11-24 19:46:29 +0530

    shop module quantity implementation

--- src/app/components/store/shop/shop.component.ts
+++ src/app/components/store/shop/shop.component.ts
@@ -48,11 +48,14 @@ export class ShopComponent implements OnInit {
   type: any = null;
   category: any;
   filterTitle: string;
-  productNameTitle: string="All Products";
-  bundlePrice:any=0;
-  showActualPrice: boolean= false;
+  productNameTitle: string = "All Products";
+  bundlePrice: any = 0;
+  showActualPrice: boolean = false;
   tooltipData: boolean = false;
-  orderType : any;
+  orderType: any;
+
+  showOtherTextbox = null;
+  cartItems: any[] = [];
 
   constructor(
     private sessionService: SessionService,
@@ -126,7 +129,12 @@ export class ShopComponent implements OnInit {
         id: '4',
         name: 'Qty 4',
         value: 4
-      }
+      },
+      {
+        id: 'Others',
+        name: 'Custom',
+        value: 11,
+      },
     ]
   }
 
@@ -137,7 +145,7 @@ export class ShopComponent implements OnInit {
       if (params['type']) {
         const type = params['type'];
         this.type = type.replace(new RegExp('-', 'g'), ' ');
-        this.productNameTitle=this.type;
+        this.productNameTitle = this.type;
       }
       if (this.type == "Comfort Patch") {
         this.showAgePopUp = true;
@@ -163,23 +171,20 @@ export class ShopComponent implements OnInit {
           webCategoryDescription: element.webCategoryDescription
         });
       });
-      const category = this.categoryModels.find(x=> x.webCategoryDescription == this.type);
+      const category = this.categoryModels.find(x => x.webCategoryDescription == this.type);
       this.category = category != null ? category.webCategoryID : 0;
-      this.shopService.GetProductsList(this.category , this.filterValue).subscribe(result => {
+      this.shopService.GetProductsList(this.category, this.filterValue).subscribe(result => {
         this.shopProductModels = result;
         this.spinner.hide();
       });
     })
   }
 
-
-  showTooltip()
-  {
+  showTooltip() {
     this.tooltipData = true;
   }
 
-  closeTooltip()
-  {
+  closeTooltip() {
     this.tooltipData = false;
   }
 
@@ -194,8 +199,9 @@ export class ShopComponent implements OnInit {
         });
       });
       var data = this.categoryModels.find(x => x.webCategoryDescription.toString() === "All Products");
-      if(data){
-      data.webCategoryDescription="Select Category";}
+      if (data) {
+        data.webCategoryDescription = "Select Category";
+      }
       this.categoryId = data?.webCategoryID;
       this.category = this.type != null ? parseInt(this.type) : this.categoryId;
       this.GetProductsList(this.categoryId, this.filterValue);
@@ -208,19 +214,17 @@ export class ShopComponent implements OnInit {
     this.categoryId = this.category;
     if (this.categoryId == 39) {
       this.showAgePopUp = true;
-
-
     }
     const category = this.categoryModels.find(x => x.webCategoryID == this.categoryId);
     const categoryName = category.webCategoryDescription.replace(new RegExp(' ', 'g'), '-');
-    this.productNameTitle= categoryName;
+    this.productNameTitle = categoryName;
     this.sessionService.setSessionItem('categoryDescription', categoryName);
     this.router.navigate([`store/products/${categoryName}`])
   }
 
   open(content: any, product: any, adultCheck: any) {
     //this.bundle = 'single';
-    this.showActualPrice= false;
+    this.showActualPrice = false;
     if (this.showAgePopUp == true) {
       this.reProduct = product;
       this.modalService.open(adultCheck, this.modalOptions).result.then((result) => {
@@ -228,7 +232,7 @@ export class ShopComponent implements OnInit {
         alert(this.closeResult);
       }, (reason) => {
         this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        this.sessionService.setSessionObject("ignore_Comfort_patch","ComfortPatch");
+        this.sessionService.setSessionObject("ignore_Comfort_patch", "ComfortPatch");
       });
     } else {
       this.product = product;
@@ -280,14 +284,14 @@ export class ShopComponent implements OnInit {
     return `${environment.productImageApiUrl}${imageName}`;
   }
 
-  RedirectToProduct(adultCheck:any,product: any,learnMore:string) {
-    if(learnMore=="learnMore"){
+  RedirectToProduct(adultCheck: any, product: any, learnMore: string) {
+    if (learnMore == "learnMore") {
       this.showAgePopUp = false;
       this.modalService.dismissAll();
       this.router.navigate(['/store/product', product.itemCode]);
     }
-    this.reProduct=product;
-    if(this.category == 22){
+    this.reProduct = product;
+    if (this.category == 22) {
       this.sessionService.setSessionItem('categoryDescription', "All-Products");
     }
     if (this.showAgePopUp == true) {
@@ -295,10 +299,10 @@ export class ShopComponent implements OnInit {
         this.closeResult = `Closed with: ${result}`;
       }, (reason) => {
         this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        this.sessionService.setSessionObject("ignore_Comfort_patch","ComfortPatch");
+        this.sessionService.setSessionObject("ignore_Comfort_patch", "ComfortPatch");
 
       });
-    } else{
+    } else {
       this.modalService.dismissAll();
       this.router.navigate(['/store/product', product.itemCode]);
     }
@@ -311,7 +315,7 @@ export class ShopComponent implements OnInit {
   addToCart(product: any) {
     debugger;
 
-    if(this.sessionService.getSessionObject("ignore_Comfort_patch")){
+    if (this.sessionService.getSessionObject("ignore_Comfort_patch")) {
       this.toastrService.error('Sorry You are Under 18.');
       return;
     }
@@ -331,7 +335,7 @@ export class ShopComponent implements OnInit {
       discount: 0,
       quantityLimit: 4,
       isDisabled: null,
-      orderType:0
+      orderType: 0
 
     }
     Object.entries(items).forEach(([key, value]) => { product[key] = value });
@@ -376,8 +380,11 @@ export class ShopComponent implements OnInit {
           product.quantityModel = single_singledelivery.quantityModel + +product.quantityModel;
 
           if (this.product.quantityModel > product.quantityLimit) {
-            this.productItems.push(old_single_singledelivery);
-            this.toastrService.error('You Exceed your Quantity Limit 4');
+            // this.productItems.push(old_single_singledelivery);
+            // this.toastrService.error('You Exceed your Quantity Limit 4');
+            
+            this.productItems.push(product);
+            this.toastrService.success('Product added successfully');
           }
           else {
             this.productItems.push(product);
@@ -403,8 +410,11 @@ export class ShopComponent implements OnInit {
           product.quantityModel = single_subscriptiondelivery.quantityModel + +product.quantityModel;
 
           if (product.quantityModel > product.quantityLimit) {
-            this.productItems.push(old_single_subscriptiondelivery);
-            this.toastrService.error('You Exceed your Quantity Limit 4');
+            // this.productItems.push(old_single_subscriptiondelivery);
+            // this.toastrService.error('You Exceed your Quantity Limit 4');
+            
+            this.productItems.push(product);
+            this.toastrService.success('Product added successfully');
           }
           else {
             this.productItems.push(product);
@@ -430,8 +440,11 @@ export class ShopComponent implements OnInit {
           product.quantityModel = multiple_singledelivery.quantityModel + +product.quantityModel;
 
           if (product.quantityModel > product.quantityLimit) {
-            this.productItems.push(old_multiple_singledelivery);
-            this.toastrService.error('You Exceed your Quantity Limit 4');
+            // this.productItems.push(old_multiple_singledelivery);
+            // this.toastrService.error('You Exceed your Quantity Limit 4');
+            
+            this.productItems.push(product);
+            this.toastrService.success('Product added successfully');
           }
           else {
             this.productItems.push(product);
@@ -457,8 +470,11 @@ export class ShopComponent implements OnInit {
           product.quantityModel = multiple_subscriptiondelivery.quantityModel + +product.quantityModel;
 
           if (product.quantityModel > product.quantityLimit) {
-            this.productItems.push(old_multiple_subscriptiondelivery);
-            this.toastrService.error('You Exceed your Quantity Limit 4');
+            // this.productItems.push(old_multiple_subscriptiondelivery);
+            // this.toastrService.error('You Exceed your Quantity Limit 4');
+            
+            this.productItems.push(product);
+            this.toastrService.success('Product added successfully');
           }
           else {
             this.productItems.push(product);
@@ -486,19 +502,18 @@ export class ShopComponent implements OnInit {
   }
 
   checkBundle(bundle: string, productPrice: any) {
-    this.bundlePrice=0;
+    this.bundlePrice = 0;
     this.bundle = bundle;
-    if(bundle=='single')
-    {
-     this.bundlePrice=productPrice;
+    if (bundle == 'single') {
+      this.bundlePrice = productPrice;
     }
-    else{
+    else {
       //let itemPrice = productPrice;
-      this.showActualPrice=true;
+      this.showActualPrice = true;
       let itemPrice = productPrice * 2;
       let discountper5 = (itemPrice * 5) / 100;
       this.bundlePrice = itemPrice - discountper5;
-      this.bundlePrice =this.bundlePrice.toFixed(2);
+      this.bundlePrice = this.bundlePrice.toFixed(2);
     }
   }
 
@@ -521,5 +536,45 @@ export class ShopComponent implements OnInit {
     this.spinner.show();
     this.filterValue = parseInt(event.target.value);
     this.GetProductsList(this.categoryId, this.filterValue);
+  }
+
+  quantityForOneTime(productDetail: any, selectedvalue: number) {
+    debugger;
+    this.quantityCalculation(productDetail, selectedvalue);
+  }
+
+  quantitychanged(cartitem: any, selectedvalue: number) {
+    debugger;
+    this.quantityCalculation(cartitem, selectedvalue);
+    const item = this.cartItems.find((x) => x.itemCode == cartitem.itemCode);
+
+    if (item && selectedvalue > 5) {
+      item.quantityModel = +selectedvalue;
+    }
+  }
+
+  quantityCalculation(productDetail: any, selectedvalue: number) {
+    debugger;
+    this.cartItems = this.sessionService.getSessionObject('productCartItems');
+    if (selectedvalue < 5) {
+      productDetail.extraQuantity = null;
+    }
+    if (selectedvalue != null && selectedvalue != undefined) {
+      if (selectedvalue == 11 || selectedvalue > 5) {
+        this.showOtherTextbox = productDetail.itemCode;
+        $('#showinput' + productDetail.itemCode).show();
+      } else {
+        $('#showinput' + productDetail.itemCode).hide();
+      }
+      if (this.cartItems) {
+        for (var i = 0; i <= this.cartItems.length - 1; i++) {
+          if (this.cartItems[i].itemCode == productDetail.itemCode) {
+            this.cartItems[i].quantityModel = +selectedvalue;
+          }
+        }
+      }
+    }
+    this.product.quantityModel = +selectedvalue;
+    this.sessionService.setSessionObject('productCartItems', this.cartItems);
   }
 }
