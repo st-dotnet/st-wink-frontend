ee21b8b5714a07b2c2e5192f69cd099bf93b94b8
stdev <stdev.net@gmail.com>
2021-12-14 18:21:35 +0530

    enrollment-UI-changes

--- src/app/components/home/enrollment/enrollmentinfo/enrollmentinfo.component.ts
+++ src/app/components/home/enrollment/enrollmentinfo/enrollmentinfo.component.ts
@@ -9,7 +9,29 @@ export class EnrollmentInfoComponent implements OnInit {
     addrnew = false;
     ngOnInit(): void {
     }
+    
+    isCardDisabled: boolean = false;
+    isCardType: number;
+    newAddress: any;
 
+    onSelectCardChange(id: number) {
+      switch (id) {
+        case 1:
+          //prepare existing card value;
+          this.isCardDisabled = false;
+          this.isCardType = id;
+          //console.log(id);
+  
+          break;
+        case 2:
+          // take new card value and create token and store it to database and get;
+          this.isCardDisabled = true;
+          this.isCardType = id;
+          break;
+        default:
+          break;
+      }
+    }
     
   shippingAddressType(billingAddressType: string) {
     if (billingAddressType == 'shippingAddress') {
@@ -18,5 +40,6 @@ export class EnrollmentInfoComponent implements OnInit {
       this.addrnew = true;
     }
   }
+  
   
 }
