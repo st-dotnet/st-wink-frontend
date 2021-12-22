0517c4a2702921a4d5b6e6d32f4c4bd9f0c6a842
stdev <stdev.net@gmail.con>
2021-12-22 17:01:33 +0530

    address changes

--- src/app/components/auth/account-settings/addresses/addresses.component.ts
+++ src/app/components/auth/account-settings/addresses/addresses.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionService } from '@app/_services';
+import { ShopService } from '@app/_services/shop.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-addresses',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addresses.component.css']
 })
 export class AddressesComponent implements OnInit {
+  user: any;
+  addressess:any[]=[];
+  constructor(private shopService: ShopService,  private sessionService: SessionService,
+    private router: Router,
+    private spinner: NgxSpinnerService,
+    private toastrService: ToastrService) { }
 
-  constructor() { }
-
+   
   ngOnInit(): void {
+    this.user = this.sessionService.getSessionObject('user');
+    if(this.user){
+      this.getAddressByCustomerId(this.user.customerId);
+    }
+  
   }
+  getAddressByCustomerId(id) {
+    debugger;
+    this.spinner.show();
+    this.shopService.getCustomerAddressById(id).subscribe((result: any) => {
+   this.spinner.hide();
+      if (result!=null) {
+      this.addressess = result?.result?.customers;   
+      }});
+  };
 
 }
